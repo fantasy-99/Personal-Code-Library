ce293723413be0af46398684e39307349d2f929e
956256648 <956256648@qq.com>
2022-11-27 16:53:55 +0800

    C4.5

---
+++ Machine Learning/C4.5.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+
+#注意，未设计剪枝
+#增益率会对可取值数目较小的特征有偏好，为了避免这个问题，C4.5并不是直接使用增益率的大小进行划分特征，而是先从候选划分特征中找出信息增益高于平均水平的属性，再从中选择增益率最高的那个特征。
+
+#计算信息熵
+def cal_information_ent(data):
+    data_label = data.iloc[:, -1]
+    label_class = data_label.value_counts()
+    Ent = 0
+    for k in label_class.keys():
+        pk = label_class[k] / len(data_label)
+        Ent += -pk * np.log2(pk)
+    return Ent
+
+#计算属性a的信息增益和增益率
+def cal_information_gain(data, a):
+    Ent = cal_information_ent(data) #先算该点的信息熵
+    feature_class = data[a].value_counts() #属性可能取值的数量
+    gain = 0
+    IV_a = 0
+    for v in feature_class.keys():
+        weight = feature_class[v] / data.shape[0]
+        Ent_v = cal_information_ent(data.loc[data[a] == v])
+        gain += weight * Ent_v
+        IV_a += (-weight) * np.log2(weight)
+    temp = Ent - gain
+    gain_ration = temp / IV_a
+    return (temp, gain_ration)
+
+#选出样本数量最多的类
+def get_most_label(data):
+    data_label = data.iloc[:, -1]
+    label_sort = data_label.value_counts(sort=True)
+    return label_sort.keys()[0]
+
+#挑选最优属性，在信息增益大于平均水平的属性中选取增益率最高的
+def get_best_feature(data):
+    #取出当前所有属性
+    features = data.columns[:-1]
+    res = {}
+    for a in features:
+        res[a] = cal_information_gain(data, a) #信息增益和增益率
+    res = sorted(res.items(), key=lambda x: x[1][0], reverse=True) #按信息增益排名，items表示把字典转换成列表， x[1]表示按value排序，而且保留key和value
+    res_avg = sum([x[1][0] for x in res])/len(res) #信息增益平均值
+    good_res = [x for x in res if x[1][0] >= res_avg] #高于平均水平的
+    res = sorted(good_res, key=lambda x: x[1][1], reverse=True) #按增益率排序
+    return res[0][0]
+
+#将数据转化为（属性值：数据表）的元组形式返回，并删去之前的属性列
+def drop_exist_feature(data, best_feature):
+    attr = pd.unique(data[best_feature])
+    new_data = [(nd, data[data[best_feature] == nd]) for nd in attr]
+    new_data = [(n[0], n[1].drop([best_feature], axis=1)) for n in new_data]
+    return new_data
+
+#创建决策树，递归
+def create_tree(data):
+    data_label = data.iloc[:, -1]
+    #只有一类了，叶节点
+    if len(data_label.value_counts()) == 1:
+        return data_label.values[0]
+
+    #所有样本所有属性的取值相同，选样本数量最多的类为叶节点
+    if all(len(data[i].value_counts()) == 1 for i in data.iloc[:, :-1].columns):
+        return get_most_label(data)
+
+    #根据信息增益得到的最优属性划分
+    best_feature = get_best_feature(data)
+    Tree = {best_feature: {}}  #字典形式存储
+    exist_vals = pd.unique(data[best_feature]) #最优属性的取值
+
+    #如果属性的取值减少了，即某个取值的样本集合为空，则标记为叶节点，类别为样本集中样本数最多的类
+    if len(exist_vals) != len(column_count[best_feature]):
+        no_exist_attr = set(column_count[best_feature]) - set(exist_vals)
+        for no_feat in no_exist_attr:
+            Tree[best_feature][no_feat] = get_most_label(data)
+
+    #根据属性值创建决策子树
+    for item in drop_exist_feature(data, best_feature): #item[1]其实就是data
+        Tree[best_feature][item[0]] = create_tree(item[1])
+    return Tree
+
+#预测
+def predict(Tree, test_data):
+    #从决策树根节点开始
+    first_feature = list(Tree.keys())[0] #属性
+    second_dict = Tree[first_feature] #当前节点的子树
+    input_first = test_data.get(first_feature) #属性值
+    input_value = second_dict[input_first] #选择对应子节点的子树
+    if isinstance(input_value, dict): #判断是否是字典，即是不是非叶节点
+        class_label = predict(input_value, test_data)
+    else:
+        class_label = input_value
+    return class_label
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('data_word.csv') #github有，17个样本
+    #统计每个特征的取值情况作为全局变量,以字典形式存储
+    column_count = dict([(ds, list(pd.unique(data[ds]))) for ds in data.iloc[:, :-1].columns])
+
+    #创建决策树
+    decision_tree = create_tree(data)
+    print(decision_tree)
+    test_data = {'色泽': '青绿', '根蒂': '蜷缩', '敲声': '浊响', '纹理': '稍糊', '脐部': '凹陷', '触感': '硬滑'}
+    result = predict(decision_tree, test_data)
+    print('好瓜' if result == 1 else '坏瓜')
